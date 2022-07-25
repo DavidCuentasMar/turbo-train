867bbacf8715ae04da0e119ec971d1e2547c2dfc
DavidCuentasMar <davidandrescuentasmar@gmail.com>
2022-07-25 18:38:44 -0500

    gradingStudents,kangaroo

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main{
+
+    public static void main(String[] args){
+        List<Integer> grades = new ArrayList<Integer>();
+        System.out.println(gradingStudents(grades));
+    }
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        List<Integer> newGrades = new ArrayList<Integer>();
+        for (Integer gradNum : grades) {
+            if(gradNum >= 38){
+                gradNum +=  gradNum % 5 >= 3 ? (5 - gradNum % 5) : 0;
+            }
+            newGrades.add(gradNum);
+        }
+        return newGrades;
+    }
+    
+    public static String kangaroo(int x1, int v1, int x2, int v2) {
+        return v2 < v1 && (x2 - x1) % (v1 - v2) == 0 ? "YES" : "NO";
+
+    }
+}
