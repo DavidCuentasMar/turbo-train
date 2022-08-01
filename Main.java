8b522f4edaf5d09a47c507f596588319de2b4197
DavidCuentasMar <davidandrescuentasmar@gmail.com>
2022-08-01 15:24:02 -0500

    breakingRecords

--- Main.java
+++ Main.java
@@ -1,10 +1,35 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main{
 
     public static void main(String[] args){
         System.out.println("GG");
+        System.out.println(breakingRecords(Arrays.asList(10,5,20,20,4,5,2,25,1)).get(0));
+        System.out.println(breakingRecords(Arrays.asList(10,5,20,20,4,5,2,25,1)).get(1));
+    }
+
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+        Integer[] records = new Integer[2];
+        records[0] = 0;
+        records[1] = 0;
+        int max = scores.get(0);
+        int min = scores.get(0);
+        for(Integer i=1;i<scores.size(); i++){
+            if(scores.get(i) > max){
+                max = scores.get(i);
+                records[0]+=1;
+            }
+            if(scores.get(i) < min){
+                min = scores.get(i);
+                records[1]+=1;
+
+            }
+        }
+        return Arrays.asList(records);
+    }
+
+    public static String misereNim(List<Integer> s) {
+        // TODO
     }
 
     public static String timeConversion(String s) {
